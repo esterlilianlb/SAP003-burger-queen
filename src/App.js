9aa4ea20b64d5d993d4f411262132a59b5d80904
esterlilianlb <esterlilian2231@gmail.com>
2019-12-17 16:21:44 -0300

    config inicial firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header.js';
+import menu from './pages/initial.js'
+// import {db} from './components/firebase.js'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Header />
+      {menu}
     </div>
   );
 }
 
 export default App;
+
