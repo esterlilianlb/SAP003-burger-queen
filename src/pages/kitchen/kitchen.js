25da0794588d6b530320c44e9885bd1b456a64e2
esterlilianlb <esterlilian2231@gmail.com>
2020-01-14 17:18:38 -0300

    pedidos para a cozinha

--- src/pages/kitchen/kitchen.js
+++ src/pages/kitchen/kitchen.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Nav from '../../components/Nav/Nav'
+import db from '../../utils/config'
 
 function Kitchen() {
+
+  const [ordersToDo, setOrdersToDo] = useState([]);
+  
+  useEffect(() => {
+    db.collection('pedidos').get().then(snapshot=> 
+    snapshot.forEach(doc => {
+        return setOrdersToDo(currentSate => [...currentSate, doc.data()])
+          
+    }))
+    
+}, []);
     return(
       <div>
+        
         <div className="nav"><Nav/></div>
-        <h1>Pedidos para a cozinha</h1>
+    {ordersToDo.map((item, index)=> 
+      <div key={index}>
+        <p>Nome: {item.clientName}</p>
+        <p>Mesa: {item.table}</p>
+        {item.order.forEach(item => <p>Pedido: {item}</p>)}
+        <p>Status: {item.status}</p>
+        <p>Hora do pedido: {item.timestamp}</p>
+
+      </div>
+    )}
       </div>
         )
 }
